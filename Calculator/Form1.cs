f5ab07d9ca397f6b1ad5dcec6b4e4b80ec3dba57
Ciprian Craciun <ciprian.craciun@cgm.com>
2020-03-22 16:40:38 +0200

    create calculator

---
+++ Calculator/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class Form1 : Form
+    {
+        char[] signs;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClick(object sender, EventArgs e)
+        {
+            Button btn = sender as Button;
+            if (btn != null)
+            {
+                this.textBox1.Text += btn.Text;
+            }
+        }
+
+        private void Calculate(object sender, MouseEventArgs e)
+        {
+            string text = this.textBox1.Text;
+            bool containsAnyLetter = signs.Any(c => this.textBox1.Text.Contains(c));
+            if (containsAnyLetter)
+            {
+                string[] words = text.Split(signs);
+                var sum = 0;
+                foreach (var word in words)
+                {
+                    var wordInt = Convert.ToInt32(word);
+                    int index = 0;
+                    foreach (var sign in signs)
+                    {
+                        switch (sign)
+                        {
+                            case '+':
+                                sum += wordInt;
+                                this.textBox1.Clear();
+                                this.textBox1.AppendText(Convert.ToString(sum));
+                                break;
+                            case '-':
+                                if (index + 1 < words.Length)
+                                    sum = Convert.ToInt32(words[0]) - Convert.ToInt32(words[index + 1]);
+                                this.textBox1.Clear();
+                                this.textBox1.AppendText(Convert.ToString(sum));
+                                break;
+                            case '*':
+                                if (index + 1 < words.Length)
+                                    sum = Convert.ToInt32(words[0]) * Convert.ToInt32(words[index + 1]);
+                                this.textBox1.Clear();
+                                this.textBox1.AppendText(Convert.ToString(sum));
+                                break;
+                            case '/':
+                                if (index + 1 < words.Length)
+                                    sum = Convert.ToInt32(words[0]) / Convert.ToInt32(words[index + 1]);
+                                this.textBox1.Clear();
+                                this.textBox1.AppendText(Convert.ToString(sum));
+                                break;
+                            default:
+                                Console.WriteLine($"An unexpected value ({sign})");
+                                break;
+                        }
+                        index++;
+                    }
+                }
+            }
+        }
+
+        private void btnClickSign(object sender, EventArgs e)
+        {
+            Button btn = sender as Button;
+            if (btn != null)
+            {
+                this.textBox1.Text += btn.Text;
+                signs = (btn.Text).ToCharArray();
+            }
+        }
+
+        private void btnRemove(object sender, EventArgs e)
+        {
+            this.textBox1.Clear();
+        }
+
+        private void keyPress(object sender, KeyPressEventArgs e)
+        {
+            Button btn = sender as Button;
+            if (e.KeyChar == '+')
+            {
+                Console.WriteLine($"key press is + {btn.Text}");
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# tspdotnet-lab2
+# tspdotnet-lab1
+
+WF calculator personal
+
+Am atasat un gif pentru modul de functionare a aplicatiei, nu se pot efectua operatii complexe deoarece mai sunt unele bug-uri nefixate:
+
+![Calculator](calculator.gif "Calculator")
